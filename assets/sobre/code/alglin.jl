6664b8c4de8a4313ada7005931064e303e82692f
davibarreira <davisbarreira@gmail.com>
2022-01-21 18:17:39 +0000

    Deploying to gh-pages from  @ 0fae7bde4a345de8163c6d465eaee7c7aa9dc7c7 🚀

--- assets/sobre/code/alglin.jl
+++
@@ -1,5 +0,0 @@
-# This file was generated, do not modify it. # hide
-using LinearAlgebra
-A = rand(10,10)
-x = rand(10)
-A * x
--- assets/sobre/code/plot.jl
+++
@@ -1,5 +0,0 @@
-# This file was generated, do not modify it. # hide
-# Plotando Função
-using Plots
-plt = plot([cos, sin], -π/2, 2π);
-savefig(plt, joinpath(@OUTPUT, "plt.svg")) # hide
